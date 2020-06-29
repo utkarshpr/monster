63bd82fea89d9ba321178b29148b2527032bec55
utkarshpr <1705089@kiit.ac.in>
2020-06-29 12:50:54 +0530

    Updates

---
+++ precache-manifest.f4c805009f7c574fc64cc431355cebf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1a122cb68c710e58bfafcb4e7858ed",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "12fdf34429933b3fa41e",
+    "url": "/monster/static/css/main.7a87bb7f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12fdf34429933b3fa41e",
+    "url": "/monster/static/js/main.1008866c.chunk.js"
+  },
+  {
+    "revision": "627926c7c91a8f7b6f29",
+    "url": "/monster/static/js/runtime-main.d18ebdc4.js"
+  }
+]);
